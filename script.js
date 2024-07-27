0014b02868872f8fe85745304a936a8e44423fdc
Gui <guilhermerebellato7@gmail.com>
2024-07-26 22:19:50 -0300

    Fix: icons errors

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleMode() {
 
   if (html.classList.contains("light")) {
     img.setAttribute("src", "/assets/avatar-light.png");
-    img.setAttribute("alt", "Foto de perfil com óculos escuro");
+    img.setAttribute("alt", "Foto de perfil com oculos escuro");
   } else {
     img.setAttribute("src", "/assets/avatar.png");
     img.setAttribute("alt", "Foto de perfil");
